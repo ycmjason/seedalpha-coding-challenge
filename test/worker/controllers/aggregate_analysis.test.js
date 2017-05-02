7108b017fd709f06367b2efdd71dffe325022b59
Jason Yu <ycm.jason@gmail.com>
2017-05-02 18:29:58 +0100

    backend should have finished

--- test/worker/controllers/aggregate_analysis.test.js
+++ test/worker/controllers/aggregate_analysis.test.js
@@ -8,9 +8,10 @@ var Email = require('../../../lib/models').Email;
 var queue = require('../../../lib/queue');
 var createEmail = require('../../../lib/server/controllers/createEmail');
 
-
+/* no clue how to test this... lol
 describe('aggregate analysis worker controller', function(){
   it('# ', function(){
 
   });
 });
+*/
